a208d993803820de76af3aabf55f8c7c7daae8b6
Carlos Saito <carlos.saito@optimizely.com>
2025-04-01 15:16:40 +0200

    Add simple config

---
+++ samples/nextjs-template/src/components/Article.tsx
@@ -0,0 +1,9 @@
+import { contentType } from 'optimizely-cms-sdk';
+
+export const ContentType = contentType({
+  key: 'Landing',
+  displayName: 'Landing page',
+  baseType: 'experience',
+});
+
+export default function X() {}
