fa786d91092279cd2d04ff8170b68d6ffc5402ab
Carlos Saito <carlos.saito@optimizely.com>
2025-04-01 11:20:37 +0200

    Export members

--- packages/optimizely-cms-sdk/src/index.ts
+++ packages/optimizely-cms-sdk/src/index.ts
@@ -1,1 +1,4 @@
 export { defineContentType } from './model';
+export { type InferFromContentType } from './infer';
+export * as ContentTypes from './model/contentTypes';
+export * as ContentTypeProperties from './model/contentTypeProperties';
