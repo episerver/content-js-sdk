f1baf23cc110beb24af99bfd151ce89a6a875720
Carlos Saito <carlos.saito@optimizely.com>
2025-04-04 13:28:17 +0200

    Add font

--- samples/nextjs-template/src/app/layout.tsx
+++ samples/nextjs-template/src/app/layout.tsx
@@ -1,6 +1,15 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { Bodoni_Moda, Inter } from 'next/font/google';
+
+const serifFont = Bodoni_Moda({
+  variable: '--font-serif',
+  subsets: ['latin'],
+});
+
+const sansFont = Inter({
+  variable: '--font-sans',
+  subsets: ['latin'],
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={[serifFont.variable, sansFont.variable].join(' ')}
+    >
       <body>{children}</body>
     </html>
   );
