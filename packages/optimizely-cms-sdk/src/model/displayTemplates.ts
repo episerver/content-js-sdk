198fc937a80cf3c76bad07f41830cce774677bb9
tromesh <tharakaromesh@gmail.com>
2025-04-07 23:59:03 +0200

    Feature:CMS-41252 - Add DisplayTemplate type

---
+++ packages/optimizely-cms-sdk/src/model/displayTemplates.ts
@@ -0,0 +1,47 @@
+import { AnyContentType } from './contentTypes';
+
+type NodeType = 'row' | 'colunm';
+
+type EditorType = 'select' | 'checkbox';
+
+type BaseType = AnyContentType['baseType'];
+
+type NodeTemplate = {
+  nodeType: NodeType;
+  baseType?: never;
+  contentType?: never;
+};
+
+type BaseTemplate = {
+  baseType: BaseType;
+  contentType?: string;
+  nodeType?: never;
+};
+
+type ChoiceType = Record<
+  string,
+  {
+    displayName: string;
+    sortOrder: number;
+  }
+>;
+
+type SettingsType = Record<
+  string,
+  {
+    displayName: string;
+    editor: EditorType;
+    sortOrder: number;
+    choices: ChoiceType;
+  }
+>;
+
+type BaseDisplayTemplate = {
+  key: string;
+  displayName?: string;
+  isDefault: boolean;
+  settings: SettingsType;
+};
+
+export type DisplayTemplate = BaseDisplayTemplate &
+  (NodeTemplate | BaseTemplate);
--- packages/optimizely-cms-sdk/src/model/index.ts
+++ packages/optimizely-cms-sdk/src/model/index.ts
@@ -1,6 +1,12 @@
 import { AnyContentType } from './contentTypes';
+import { DisplayTemplate } from './displayTemplates';
 
 /** Defines a Optimizely CMS content type */
 export function contentType<T extends AnyContentType>(options: T): T {
+  return options;
+}
+
+/** Defines a Optimizely CMS display template */
+export function displayTemplate<T extends DisplayTemplate>(options: T): T {
   return options;
 }
