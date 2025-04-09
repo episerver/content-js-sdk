b972d2493a0750fd0d4e2b36a836bd0f030b64d7
Carlos Saito <carlos.saito@optimizely.com>
2025-04-09 11:27:35 +0200

    Create test fixtures

---
+++ packages/optimizely-cms-sdk/src/graph/__test__/fixtures.ts
@@ -0,0 +1,42 @@
+import { contentType } from '../../model';
+
+export const callToAction = contentType({
+  baseType: 'component',
+  key: 'CallToAction',
+  properties: {
+    label: { type: 'string' },
+    link: { type: 'string' },
+  },
+});
+
+export const heroBlock = contentType({
+  baseType: 'component',
+  key: 'Hero',
+  properties: {
+    heading: { type: 'string' },
+    callToAction: {
+      type: 'array',
+      items: { type: 'content', views: [callToAction] },
+    },
+  },
+});
+
+export const landingPage = contentType({
+  baseType: 'page',
+  key: 'LandingPage',
+  properties: {
+    hero: { type: 'content', views: [heroBlock] },
+    body: { type: 'richText' },
+  },
+});
+
+export const articlePage = contentType({
+  baseType: 'page',
+  key: 'ArticlePage',
+  properties: {
+    body: { type: 'richText' },
+    relatedArticle: { type: 'link' },
+    source: { type: 'url' },
+    tags: { type: 'array', items: { type: 'string' } },
+  },
+});
