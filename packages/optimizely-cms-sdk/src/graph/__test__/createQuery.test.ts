9df4fb4c384eb617bc8c86b9b87c45701d8099e6
Carlos Saito <carlos.saito@optimizely.com>
2025-04-11 09:30:18 +0200

    Rename createFragment > createQuery

--- packages/optimizely-cms-sdk/src/graph/__test__/createQuery.test.ts
+++ packages/optimizely-cms-sdk/src/graph/__test__/createQuery.test.ts
@@ -1,6 +1,37 @@
 import { describe, expect, test } from 'vitest';
-import { createQuery } from '..';
-import { articlePage, callToAction, customImport, heroBlock } from './fixtures';
+import { createFragment, createQuery } from '../createQuery';
+import { callToAction, heroBlock, landingPage, customImport } from './fixtures';
+
+describe('createFragment()', () => {
+  test('works for simple properties', async () => {
+    const result = await createFragment(callToAction.key, customImport);
+    expect(result).toMatchInlineSnapshot(`
+      "
+      fragment CallToAction on CallToAction { label link }"
+    `);
+  });
+
+  test('works for components inside components', async () => {
+    const result = await createFragment(heroBlock.key, customImport);
+    expect(result).toMatchInlineSnapshot(`
+      "
+      fragment CallToAction on CallToAction { label link }
+      fragment Hero on Hero { heading callToAction { __typename ...CallToAction } }"
+    `);
+  });
+
+  test('works for components inside components (several levels)', async () => {
+    const result = await createFragment(landingPage.key, customImport);
+    expect(result).toMatchInlineSnapshot(`
+      "
+      fragment CallToAction on CallToAction { label link }
+      fragment Hero on Hero { heading callToAction { __typename ...CallToAction } },
+      fragment CallToAction on CallToAction { label link }
+      fragment SuperHero on SuperHero { heading embed_video callToAction { __typename ...CallToAction } }
+      fragment LandingPage on LandingPage { hero { __typename ...Hero ...SuperHero } body { html, json } }"
+    `);
+  });
+});
 
 describe('createQuery', () => {
   test('simple content types', async () => {
