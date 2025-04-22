ccd6a6c529a9136e9c5ecfd18575d359db9c9c85
Carlos Saito <carlos.saito@optimizely.com>
2025-04-22 11:57:07 +0200

    Simplify rendering and GraphQL

--- samples/nextjs-template/src/app/render/[slug]/page.tsx
+++ samples/nextjs-template/src/app/render/[slug]/page.tsx
@@ -1,21 +1,6 @@
 import { GraphClient } from 'optimizely-cms-sdk/dist/graph';
-import {
-  initReactComponentRegistry,
-  OptimizelyComponent,
-} from 'optimizely-cms-sdk/dist/render/react';
+import { OptimizelyComponent } from 'optimizely-cms-sdk/dist/render/react';
 import React from 'react';
-
-initReactComponentRegistry({
-  resolver(contentType) {
-    return React.lazy(() => import(`../../../components/${contentType}.tsx`));
-  },
-});
-
-async function myImport(contentType: string) {
-  return import(`../../../components/${contentType}.tsx`).then(
-    (r) => r.ContentType
-  );
-}
 
 type Props = {
   params: Promise<{
@@ -25,7 +10,7 @@ type Props = {
 
 export default async function Page({ params }: Props) {
   const { slug } = await params;
-  const client = new GraphClient(process.env.GRAPH_SINGLE_KEY!, myImport);
+  const client = new GraphClient(process.env.GRAPH_SINGLE_KEY!);
   const c = await client.fetchContent(`/${slug}/`);
 
   return <OptimizelyComponent opti={c} />;
