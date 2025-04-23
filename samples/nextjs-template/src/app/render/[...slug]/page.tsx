bc89cce3fcda71e126edc4b89b50eef214118bcf
Carlos Saito <carlos.saito@optimizely.com>
2025-04-23 14:50:27 +0200

    Support multi-language slugs

--- samples/nextjs-template/src/app/render/[...slug]/page.tsx
+++ samples/nextjs-template/src/app/render/[...slug]/page.tsx
@@ -1,4 +1,15 @@
 import { GraphClient } from 'optimizely-cms-sdk/dist/graph';
+import {
+  initReactComponentRegistry,
+  OptimizelyComponent,
+} from 'optimizely-cms-sdk/dist/render/react';
+import React from 'react';
+
+initReactComponentRegistry({
+  resolver(contentType) {
+    return React.lazy(() => import(`../../../components/${contentType}.tsx`));
+  },
+});
 
 async function myImport(contentType: string) {
   return import(`../../../components/${contentType}.tsx`).then(
@@ -8,17 +19,18 @@ async function myImport(contentType: string) {
 
 type Props = {
   params: Promise<{
-    slug: string;
+    slug: string[];
   }>;
 };
 
 export default async function Page({ params }: Props) {
   const { slug } = await params;
+
   const client = new GraphClient(process.env.OPTIMIZELY_GRAPH_SINGLE_KEY!, {
     customImport: myImport,
     graphUrl: process.env.OPTIMIZELY_GRAPH_URL,
   });
-  const c = await client.fetchContent(`/${slug}/`);
+  const c = await client.fetchContent(`/${slug.join('/')}/`);
 
-  return <pre>{JSON.stringify(c, null, 2)}</pre>;
+  return <OptimizelyComponent opti={c} />;
 }
