ac5ce273a1a4f1725bbcfb659dfaf28ce643a2be
Carlos Saito <carlos.saito@optimizely.com>
2025-04-23 14:18:23 +0200

    Pass graph URL from sample site

--- samples/nextjs-template/src/app/graph-test/[slug]/page.tsx
+++ samples/nextjs-template/src/app/graph-test/[slug]/page.tsx
@@ -14,7 +14,10 @@ type Props = {
 
 export default async function Page({ params }: Props) {
   const { slug } = await params;
-  const client = new GraphClient(process.env.GRAPH_SINGLE_KEY!, myImport);
+  const client = new GraphClient(process.env.OPTIMIZELY_GRAPH_SINGLE_KEY!, {
+    customImport: myImport,
+    graphUrl: process.env.OPTIMIZELY_GRAPH_URL,
+  });
   const c = await client.fetchContent(`/${slug}/`);
 
   return <pre>{JSON.stringify(c, null, 2)}</pre>;
