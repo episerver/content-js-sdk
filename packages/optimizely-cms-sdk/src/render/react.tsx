80edbefd0b957e97f29e1d29f6293e2bed3ec3b0
Carlos Saito <carlos.saito@optimizely.com>
2025-04-24 11:41:43 +0200

    Restore React component registry

--- packages/optimizely-cms-sdk/src/render/react.tsx
+++ packages/optimizely-cms-sdk/src/render/react.tsx
@@ -1,5 +1,14 @@
 'use server';
-import { getContentType } from '../model/contentTypeRegistry';
+import type React from 'react';
+import { ComponentRegistry, ComponentResolver } from './component-registry';
+
+type ComponentType = React.ComponentType<any>;
+
+let componentRegistry: ComponentRegistry<ComponentType>;
+
+type InitOptions = {
+  resolver: ComponentResolver<ComponentType>;
+};
 
 type Props = {
   opti: {
@@ -7,9 +16,17 @@ type Props = {
   };
 };
 
+export function initReactComponentRegistry(options: InitOptions) {
+  componentRegistry = new ComponentRegistry(options.resolver);
+}
+
 export async function OptimizelyComponent({ opti, ...props }: Props) {
+  if (!componentRegistry) {
+    throw new Error('You should call `initReactComponentRegistry` first');
+  }
+
   const contentType = opti.__typename;
-  const Component = getContentType(contentType)?.component;
+  const Component = await componentRegistry.getComponent(contentType);
 
   if (!Component) {
     return <div>No component found for content type {contentType}</div>;
