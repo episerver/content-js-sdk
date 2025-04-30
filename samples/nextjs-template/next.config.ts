7958a223f12a2fdea11ee0f4083174ceb9e5126b
Carlos Saito <carlos.saito@optimizely.com>
2025-04-30 09:54:59 +0200

    Add support for static website

--- samples/nextjs-template/next.config.ts
+++ samples/nextjs-template/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: process.env.NODE_ENV === 'development' ? 'standalone' : 'export',
   images: {
     remotePatterns: [
       {
