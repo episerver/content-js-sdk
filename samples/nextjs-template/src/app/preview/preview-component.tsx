e4dbf93c5ab88a28f0cb678609bdabf0149f0549
Carlos Saito <carlos.saito@optimizely.com>
2025-05-12 11:45:22 +0200

    Remove console.log

--- samples/nextjs-template/src/app/preview/preview-component.tsx
+++ samples/nextjs-template/src/app/preview/preview-component.tsx
@@ -4,11 +4,9 @@ import { useEffect } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function callback(event: any) {
-  console.log('CALLBACK');
   const message = event.detail;
 
   const newPreviewUrl = message.previewUrl;
-  console.log(newPreviewUrl);
 
   window.location.replace(newPreviewUrl);
 }
@@ -16,7 +14,6 @@ function callback(event: any) {
 // This is a temporal React.js Preview component. It updates the entire page when content is saved in the CMS
 export function PreviewComponent() {
   useEffect(() => {
-    console.log('USE EFFECT');
     window.addEventListener('optimizely:cms:contentSaved', callback);
 
     return () => {
