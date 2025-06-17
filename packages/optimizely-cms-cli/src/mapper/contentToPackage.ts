7cadc9ef00341e3323f29151e9d6ca14f2e9bafa
tromesh <tharakaromesh@gmail.com>
2025-06-17 15:44:02 +0200

    validate contentType key in CLI

--- packages/optimizely-cms-cli/src/mapper/contentToPackage.ts
+++ packages/optimizely-cms-cli/src/mapper/contentToPackage.ts
@@ -1,7 +1,13 @@
 import { AnyContentType, extractKeyName } from '../service/utils.js';
+import { isKeyInvalid } from '../utils/validate.js';
 
 export function mapContentToManifest(contentTypes: AnyContentType[]): any[] {
   return contentTypes.map((contentType) => {
+    if (isKeyInvalid(contentType.key)) {
+      throw new Error(
+        `❌ [optimizely-cms-cli] Invalid content type key: "${contentType.key}". Keys must be alphanumeric and cannot start with a special character or number.`
+      );
+    }
     // Spread the contentType as extract properties
     const { properties = {} } = contentType;
 
