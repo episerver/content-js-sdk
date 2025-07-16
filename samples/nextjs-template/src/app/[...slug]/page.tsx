b801c6fadfc3ec1884dd0f975bbb5ce3031c94c5
tromesh <tharakaromesh@gmail.com>
2025-07-17 00:34:51 +0200

    update env

--- samples/nextjs-template/src/app/[...slug]/page.tsx
+++ samples/nextjs-template/src/app/[...slug]/page.tsx
@@ -11,9 +11,12 @@ type Props = {
 export default async function Page({ params }: Props) {
   const { slug } = await params;
 
-  const client = new GraphClient(process.env.OPTIMIZELY_GRAPH_SINGLE_KEY!, {
-    graphUrl: process.env.OPTIMIZELY_GRAPH_URL,
-  });
+  const client = new GraphClient(
+    process.env.NEXT_PUBLIC_OPTIMIZELY_GRAPH_SINGLE_KEY!,
+    {
+      graphUrl: process.env.NEXT_PUBLIC_OPTIMIZELY_GRAPH_URL,
+    }
+  );
   const c = await client.fetchContent(`/${slug.join('/')}/`);
 
   return <OptimizelyComponent opti={c} />;
