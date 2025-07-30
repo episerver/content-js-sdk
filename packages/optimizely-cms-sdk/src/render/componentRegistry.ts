98d84cac06d9c290fa9024ad3f2791893264811d
Carlos Saito <carlos.saito@optimizely.com>
2025-07-30 14:50:27 +0200

    Re-define the component registry API

--- packages/optimizely-cms-sdk/src/render/componentRegistry.ts
+++ packages/optimizely-cms-sdk/src/render/componentRegistry.ts
@@ -1,13 +1,37 @@
-export type ComponentResolver<C> =
-  | Record<string, C>
-  | ((contentType: string) => C);
+/** Definition of one ContentType to Component mapping */
+type ComponentFullDefinition<C> = {
+  default: C;
+  tags: Record<string, C>;
+};
+
+/** Optional arguments to resolve a component */
+type ResolverOptions = {
+  tag: string;
+};
+
+/** Function that returns a component given a content type name and options */
+type ComponentResolver<C> = (
+  contentType: string,
+  options?: ResolverOptions
+) => C;
+
+/** Maps a content type name to a component */
+type ComponentMap<C> = Record<string, C | ComponentFullDefinition<C>>;
+
+export type ComponentResolverOrObject<C> =
+  | ComponentResolver<C>
+  | ComponentMap<C>;
 
 /** A registry mapping content type names and components */
 export class ComponentRegistry<T> {
-  resolver: ComponentResolver<T>;
+  resolver: ComponentResolverOrObject<T>;
 
-  constructor(resolver: ComponentResolver<T>) {
-    this.resolver = resolver;
+  /**
+   * Initializes the component registry
+   *
+   */
+  constructor(resolverOrObject: ComponentResolverOrObject<T>) {
+    this.resolver = resolverOrObject;
   }
 
   /** Returns the component given its content type name. Returns `undefined` if not found */
