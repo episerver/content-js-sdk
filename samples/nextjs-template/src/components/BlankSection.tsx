e5523331b16f2162ae12e85b8fb1f236496e2bdd
Carlos Saito <carlos.saito@optimizely.com>
2025-08-04 11:01:15 +0200

    Add Fallback components and use them

--- samples/nextjs-template/src/components/BlankSection.tsx
+++ samples/nextjs-template/src/components/BlankSection.tsx
@@ -1,23 +1,8 @@
 import { BlankSectionContentType, Infer } from '@episerver/cms-sdk';
 import {
   OptimizelyGridSection,
-  StructureContainerProps,
   getPreviewUtils,
 } from '@episerver/cms-sdk/react/server';
-
-function Row({ children, node }: StructureContainerProps) {
-  const { pa } = getPreviewUtils(node);
-  return (
-    <div className="row" {...pa(node)}>
-      {children}
-    </div>
-  );
-}
-
-function Column({ children, node }: StructureContainerProps) {
-  const { pa } = getPreviewUtils(node);
-  return <div {...pa(node)}>{children}</div>;
-}
 
 type BlankSectionProps = {
   opti: Infer<typeof BlankSectionContentType>;
@@ -27,8 +12,8 @@ type BlankSectionProps = {
 export default function BlankSection({ opti }: BlankSectionProps) {
   const { pa } = getPreviewUtils(opti);
   return (
-    <section className="table-section" {...pa(opti)}>
-      <OptimizelyGridSection nodes={opti.nodes} row={Row} column={Column} />
+    <section {...pa(opti)}>
+      <OptimizelyGridSection nodes={opti.nodes} />
     </section>
   );
 }
