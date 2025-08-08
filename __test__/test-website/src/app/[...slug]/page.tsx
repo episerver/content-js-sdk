3b8763663a5f0a5906b9b81c42391c7b070c0923
Carlos Saito <carlos.saito@optimizely.com>
2025-08-08 11:01:22 +0200

    Update test website

--- __test__/test-website/src/app/[...slug]/page.tsx
+++ __test__/test-website/src/app/[...slug]/page.tsx
@@ -1,4 +1,4 @@
-import { GraphClient } from '@episerver/cms-sdk';
+import { GraphClient, GraphErrors } from '@episerver/cms-sdk';
 import { OptimizelyComponent } from '@episerver/cms-sdk/react/server';
 import React from 'react';
 
@@ -6,15 +6,36 @@ type Props = {
   params: Promise<{
     slug: string[];
   }>;
+  // Assume that search params are correct:
+  searchParams: Promise<{ variation?: string }>;
 };
 
-export default async function Page({ params }: Props) {
+function handleGraphErrors(err: unknown): never {
+  if (err instanceof GraphErrors.GraphResponseError) {
+    console.log('Error message:', err.message);
+    console.log('Query:', err.request.query);
+    console.log('Variables:', err.request.variables);
+  }
+  if (err instanceof GraphErrors.GraphContentResponseError) {
+    console.log('Detailed errors: ', err.errors);
+  }
+
+  throw err;
+}
+
+export default async function Page({ params, searchParams }: Props) {
   const { slug } = await params;
+  const { variation } = await searchParams;
 
   const client = new GraphClient(process.env.OPTIMIZELY_GRAPH_SINGLE_KEY!, {
     graphUrl: process.env.OPTIMIZELY_GRAPH_URL,
   });
-  const c = await client.fetchContent(`/${slug.join('/')}/`);
 
-  return <OptimizelyComponent opti={c} />;
+  const path = `/${slug.join('/')}/`;
+
+  const content = variation
+    ? await client.fetchContent({ path, variation }).catch(handleGraphErrors)
+    : await client.fetchContent(path).catch(handleGraphErrors);
+
+  return <OptimizelyComponent opti={content} />;
 }
