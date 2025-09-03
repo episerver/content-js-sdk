33a6caf0ebc3d01c65ece8eebf5a1a36182ec57b
Carlos Saito <carlos.saito@optimizely.com>
2025-09-03 10:46:01 +0200

    Add missing references test case

---
+++ __test__/test-website/src/components/missing-content-types/not-synced.tsx
@@ -0,0 +1,40 @@
+// The content types in this file ARE NOT synced to the CMS
+import { contentType } from '@episerver/cms-sdk';
+import { notInRegistry } from './not-included-in-registry';
+
+export const notSynced = contentType({
+  key: 'not_synced_to_cms',
+  baseType: '_component',
+  displayName: 'Not synced to the CMS',
+  properties: {
+    p1: { type: 'string' },
+  },
+});
+
+export const ct4 = contentType({
+  baseType: '_page',
+  displayName:
+    'test_c4: SHOULD NOT BE SYNCED. Refer to a content type that is not synecd to graph (missing in the CMS)',
+  key: 'test_c4_refs_missing',
+
+  properties: {
+    p1: {
+      type: 'content',
+      allowedTypes: [notSynced],
+    },
+  },
+});
+
+export const ct5 = contentType({
+  baseType: '_page',
+  displayName:
+    'test_c5: SHOULD NOT BE SYNCED. Refer to a content type that is not in the app registry (forgot to call "initContentTypeRegistry")',
+  key: 'test_c5_refs_missing',
+
+  properties: {
+    p1: {
+      type: 'content',
+      allowedTypes: [notInRegistry],
+    },
+  },
+});
---
+++ __test__/test-website/src/components/missing-content-types/referencing.tsx
@@ -0,0 +1,31 @@
+import { contentType } from '@episerver/cms-sdk';
+
+// This content type IS synced to the CMS.
+export const ct4 = contentType({
+  baseType: '_page',
+  displayName:
+    'test_c4: Refer to a content type that is not synecd to graph (missing in the CMS)',
+  key: 'test_c4_refs_missing',
+
+  properties: {
+    p1: {
+      type: 'content',
+      allowedTypes: ['_component'],
+    },
+  },
+});
+
+// This content type IS synced to the CMS.
+export const ct5 = contentType({
+  baseType: '_page',
+  displayName:
+    'test_c5: Refer to a content type that is not in the app registry (forgot to call "initContentTypeRegistry")',
+  key: 'test_c5_refs_missing',
+
+  properties: {
+    p1: {
+      type: 'content',
+      allowedTypes: ['_component'],
+    },
+  },
+});
