4bd1421ff8aebf6a29d0cd87f825170221563fe3
Carlos Saito <carlos.saito@optimizely.com>
2025-09-19 10:34:38 +0200

    Remove console log from middleware

--- samples/fx-integration/src/middleware.ts
+++ samples/fx-integration/src/middleware.ts
@@ -5,11 +5,11 @@ export default async function middleware(request: NextRequest) {
   const userId = request.cookies.get('user_id');
 
   if (userId) {
-    console.log('User has an ID', userId);
+    // console.log('User has an ID', userId);
     return NextResponse.next();
   } else {
     const newUserId = nanoid();
-    console.log("User doesn't have an ID. Creating new one:", newUserId);
+    // console.log("User doesn't have an ID. Creating new one:", newUserId);
     request.cookies.set('user_id', newUserId);
 
     const response = NextResponse.next();
