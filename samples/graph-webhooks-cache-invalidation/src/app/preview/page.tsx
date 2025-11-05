2a73a4384d00a984d5f70fa038419cd2be6beb5d
Carlos Saito <carlos.saito@optimizely.com>
2025-11-05 13:12:04 +0100

    Update text

--- samples/graph-webhooks-cache-invalidation/src/app/preview/page.tsx
+++ samples/graph-webhooks-cache-invalidation/src/app/preview/page.tsx
@@ -3,10 +3,9 @@ import React from 'react';
 export default function Page() {
   return (
     <div>
-      This sample site doesn&apos;t support hooks revalidation in Preview mode.
-      Go to the page in view mode by clicking{' '}
-      <strong>Options &larr; View in page</strong>
-      to test hooks revalidation
+      Preview mode doesn&apos;t cache content, to test the webhook cache
+      invalidation, navigate to view mode by clicking{' '}
+      <strong>Options &larr; View on website.</strong>
     </div>
   );
 }
