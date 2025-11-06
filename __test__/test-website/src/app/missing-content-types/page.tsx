3b070253188c4ec8c7712ae15ced4be025065e5e
tromesh <tharakaromesh@gmail.com>
2025-11-06 13:50:01 +0100

    CMS-44989 Update environment variable names and improve documentation for configuration.

--- __test__/test-website/src/app/missing-content-types/page.tsx
+++ __test__/test-website/src/app/missing-content-types/page.tsx
@@ -17,9 +17,9 @@ export default async function Page({ searchParams }: Props) {
   const { path } = await searchParams;
 
   const client = new GraphClient(process.env.OPTIMIZELY_GRAPH_SINGLE_KEY!, {
-    graphUrl: process.env.OPTIMIZELY_GRAPH_URL,
+    graphUrl: process.env.OPTIMIZELY_GRAPH_GATEWAY,
   });
-  const c = await client.fetchContent(path);
+  const c = await client.getContentByPath(path);
 
   return <pre>{c}</pre>;
 }
--- __test__/test-website/src/app/multi-host/[...slug]/page.tsx
+++ __test__/test-website/src/app/multi-host/[...slug]/page.tsx
@@ -14,7 +14,7 @@ export default async function Page({ params }: Props) {
   const { slug } = await params;
 
   const client = new GraphClient(process.env.OPTIMIZELY_GRAPH_SINGLE_KEY!, {
-    graphUrl: process.env.OPTIMIZELY_GRAPH_URL,
+    graphUrl: process.env.OPTIMIZELY_GRAPH_GATEWAY,
   });
   const host =
     process.env.NEXTJS_HOST ?? `https://localhost:${process.env.PORT ?? 3000}`;
