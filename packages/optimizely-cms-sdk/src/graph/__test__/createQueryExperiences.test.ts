5ff3db9dc8eb0c4496334dc4870cf936e8e36d5c
Carlos Saito <carlos.saito@optimizely.com>
2025-11-10 16:13:04 +0100

    Update tests

--- packages/optimizely-cms-sdk/src/graph/__test__/createQueryExperiences.test.ts
+++ packages/optimizely-cms-sdk/src/graph/__test__/createQueryExperiences.test.ts
@@ -18,7 +18,7 @@ describe('createFragment()', () => {
         "fragment MediaMetadata on MediaMetadata { mimeType thumbnail content }",
         "fragment ContentUrl on ContentUrl { type default hierarchical internal graph base }",
         "fragment IContentMetadata on IContentMetadata { key locale fallbackForLocale version displayName url {...ContentUrl} types published status created lastModified sortOrder variation ...MediaMetadata }",
-        "fragment _IContent on _IContent { _deleted _fulltext _modified _score _id _track _metadata {...IContentMetadata} }",
+        "fragment _IContent on _IContent { _id _metadata {...IContentMetadata} }",
         "fragment _IExperience on _IExperience { composition {...ICompositionNode }}",
         "fragment ICompositionNode on ICompositionNode { __typename key type nodeType displayName displayTemplateKey displaySettings {key value} ...on CompositionStructureNode { nodes @recursive } ...on CompositionComponentNode { nodeType component { ..._IComponent } } }",
         "fragment CallToAction on CallToAction { __typename label link ..._IContent }",
