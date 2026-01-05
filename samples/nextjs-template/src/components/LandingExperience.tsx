b6d12ab5ce884a7f307df340c0b1f0b1aec7e566
tromesh <tharakaromesh@gmail.com>
2026-01-05 19:04:17 +0530

    CMS-48338 Refactor image source handling in components for improved readability and consistency

--- samples/nextjs-template/src/components/LandingExperience.tsx
+++ samples/nextjs-template/src/components/LandingExperience.tsx
@@ -30,19 +30,19 @@ function ComponentWrapper({ children, node }: ComponentContainerProps) {
 
 export default function LandingExperienceComponent({ content }: Props) {
   const { pa, src } = getPreviewUtils(content);
+  const heroBackgroundUrl = src(content.hero?.background);
   return (
     <main>
       {content.hero && (
         <header className={['uni-hero', content.hero.theme].join(' ')}>
-          {(content.hero.background?.item?.Url ??
-            content.hero.background?.url.default) && (
+          {heroBackgroundUrl && (
             <Image
-              src={src(content.hero.background)}
+              src={heroBackgroundUrl}
               alt=""
               fill={true}
               {...pa('hero.background')}
             />
-          ) : null}
+          )}
           <div className="heading" {...pa('hero')}>
             <h1 {...pa('hero.heading')}>{content.hero.heading}</h1>
             <p {...pa('hero.summary')}>{content.hero.summary}</p>
