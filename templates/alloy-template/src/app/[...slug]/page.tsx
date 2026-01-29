c2d96b01591bb6a6ef4b446d2a4e8f697d2228b2
tromesh <tharakaromesh@gmail.com>
2026-01-29 12:31:29 +0100

    CMS-49011 update alloy env

--- templates/alloy-template/src/app/[...slug]/page.tsx
+++ templates/alloy-template/src/app/[...slug]/page.tsx
@@ -15,14 +15,19 @@ export default async function Page({ params }: Props) {
   const { slug } = await params;
 
   const client = new GraphClient(process.env.OPTIMIZELY_GRAPH_SINGLE_KEY!, {
-    graphUrl: process.env.OPTIMIZELY_GRAPH_URL,
+    graphUrl: process.env.OPTIMIZELY_GRAPH_GATEWAY,
   });
   const path = `/${slug.join('/')}/`;
+
+  console.log('app path', path);
+
   const pageContent = await client.getContentByPath(path);
 
   if (pageContent.length === 0) {
     notFound();
   }
+
+  console.log('app pageContent', pageContent[0]);
 
   return (
     <>
