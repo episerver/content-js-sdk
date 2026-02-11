8bdf3f4c899405d0432d7ebcb83a2a6fbf4c1a1c
tromesh <tharakaromesh@gmail.com>
2026-02-11 14:45:06 +0100

    CMS-49082 Update `/manifest` GET to include sections param and update Inbuilt propert groups

--- templates/alloy-template/src/components/base/Button.tsx
+++ templates/alloy-template/src/components/base/Button.tsx
@@ -11,13 +11,13 @@ export const ButtonContentType = contentType({
     link: {
       type: 'url',
       displayName: 'Button Link',
-      group: 'Information',
+      group: 'Content',
       required: true,
     },
     text: {
       type: 'string',
       displayName: 'Button Text',
-      group: 'Information',
+      group: 'Content',
     },
   },
   compositionBehaviors: ['elementEnabled'],
