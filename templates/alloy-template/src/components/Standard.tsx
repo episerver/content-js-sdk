23f0006a7bed153cdddea8bbfccd5c221ab20958
Tharaka Hewavitharana <tharakaromesh@gmail.com>
2026-02-12 14:17:11 +0100

    Update Standard.tsx

--- templates/alloy-template/src/components/Standard.tsx
+++ templates/alloy-template/src/components/Standard.tsx
@@ -11,7 +11,7 @@ export const StandardContentType = contentType({
   key: 'Standard',
   displayName: 'Standard Page',
   baseType: '_experience',
-  mayContainTypes: ['*'], // Passed 'News' as a string to avoid circular dependency
+  mayContainTypes: ['*'],
   properties: {
     image: {
       type: 'contentReference',
