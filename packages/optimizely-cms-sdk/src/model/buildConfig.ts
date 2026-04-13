16e08b2d7c49333734d8f3c4910ef979d9ed4808
tromesh <tharakaromesh@gmail.com>
2026-04-13 13:54:12 +0200

    CMS-51342 Add support for applications in BuildConfig type

--- packages/optimizely-cms-sdk/src/model/buildConfig.ts
+++ packages/optimizely-cms-sdk/src/model/buildConfig.ts
@@ -4,9 +4,36 @@ export type PropertyGroupType = {
   sortOrder?: number;
 };
 
+
+const applicationTypes = ['website', 'inProcessWebsite'] as const;
+const hostTypes = ['default', 'primary', 'preview', 'edit', 'media'] as const;
+
+export type HostType = (typeof hostTypes)[number];
+export type ApplicationType = (typeof applicationTypes)[number];
+
+export type ApplicationHostType = {
+  type?: HostType;
+  locale?: string;
+  authority: string;
+  preferredUrlScheme?: object;
+};
+
+export type ApplicationsType = {
+  key?: string;
+  type: ApplicationType;
+  isDefault?: boolean;
+  displayName: string;
+  entryPoint: string;
+  usePreviewTokens?: boolean;
+  hosts?: ApplicationHostType[];
+  useApplicationSpecificAssets?: boolean;
+  previewUrlFormats?: Record<string, string>;
+};
+
 export type BuildConfig = {
   components: string[];
   propertyGroups: Array<PropertyGroupType>;
+  applications: Array<ApplicationsType>;
 };
 
 // Built-in/default property groups that all users get
